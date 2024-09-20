4dedf40243902f31dba73a0d773f0272ebc52e70
Neil Douglas <neil.douglas@york.ac.uk>
2024-09-20 16:04:00 +0100

    Fixed build errors based off similar patch: https://github.com/crazyzlj/SWAT/commit/9a12d04ea1c945d01c98e43a4ee559c74b10898a.patch
    Also fixes error: "expected a right parenthesis in expression at (1)" in headout.f

--- src/HQDAV.f90
+++ src/HQDAV.f90
@@ -2,7 +2,7 @@
 !     adopted from APEX1501 by Jaehak Jeong 2017
 !     THIS SUBPROGRAM COMPUTES FLOW AREA AND DEPTH GIVEN RATE in a reach
     
-      USE PARM
+      USE PARM, except_this_one => HQDAV
       
       real*8, intent (in out) :: A, ZX, CHW, FPW
       real*8, intent (in) :: CBW, QQ, SSS, ZCH
